7398638f9fa920c1901a8f5a1f7ab17761a53823
mukt-SPEC <alaminsad6@gmail.com>
2025-12-20 17:15:35 +0100

    login ui

---
+++ lib/common/rounded_small_button.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class RoundedSmallButton extends StatelessWidget {
+  final VoidCallback onTap;
+  final String buttonText;
+  final Color buttonColor;
+  final Color textColor;
+
+  const RoundedSmallButton({
+    super.key,
+    required this.onTap,
+    required this.buttonText,
+    required this.buttonColor,
+    required this.textColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(40)),
+        color: buttonColor,
+        child: Text(buttonText, style: TextStyle(color: textColor)),
+      ),
+    );
+  }
+}
--- lib/features/auth/view/login_view.dart
+++ lib/features/auth/view/login_view.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:twitterclone/Theme/theme.dart';
+import 'package:twitterclone/common/rounded_small_button.dart';
 import 'package:twitterclone/constants/ui_constants.dart';
+
+import '../widget/auth_field.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
@@ -10,13 +14,58 @@ class LoginView extends StatefulWidget {
 
 class _LoginViewState extends State<LoginView> {
   final appBar = UiConstants.appBar();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    super.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appBar,
-      body: SingleChildScrollView(
-        padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-        child: Column(),
+      body: Center(
+        child: SingleChildScrollView(
+          padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+          child: Column(
+            spacing: 16,
+            children: [
+              AuthField(
+                key: _formKey,
+                controller: _emailController,
+                hintText: 'Email Address',
+                obscureText: false,
+              ),
+              AuthField(
+                key: _formKey,
+                controller: _passwordController,
+                hintText: 'Password',
+                obscureText: true,
+              ),
+              Align(
+                alignment: AlignmentGeometry.centerRight,
+                child: RoundedSmallButton(
+                  onTap: () {},
+                  buttonText: 'Done',
+                  buttonColor: Pallete.whiteColor,
+                  textColor: Pallete.backgroundColor,
+                ),
+              ),
+
+              RichText(
+                text: TextSpan(
+                  text: "Don't have an account? ",
+                  children: [TextSpan(text: 'Join now')],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
