422f6355e198b7f95f758901fd07ebb1f4b029eb
mukt-SPEC <alaminsad6@gmail.com>
2026-01-13 23:45:21 +0100

    little change

--- lib/features/tweets/widget/tweet_list.dart
+++ lib/features/tweets/widget/tweet_list.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:twitterclone/common/common.dart';
+import 'package:twitterclone/constants/appwrite_constant.dart';
 import 'package:twitterclone/features/tweets/controller/tweet_controller.dart';
 import 'package:twitterclone/features/tweets/widget/tweet_card.dart';
+import 'package:twitterclone/model/tweet_model.dart';
 
 class TweetList extends ConsumerWidget {
   const TweetList({super.key});
@@ -13,13 +15,27 @@ class TweetList extends ConsumerWidget {
         .watch(getTweetProvider)
         .when(
           data: (tweets) {
-            return ListView.builder(
-              itemCount: tweets.length,
-              itemBuilder: (BuildContext context, int index) {
-                final tweet = tweets[index];
-                return TweetCard(tweet : tweet);
-              },
-            );
+            return ref
+                .watch(getLatestTweetProvider)
+                .when(
+                  data: (data) {
+                    if (data.events.contains(
+                      'databases.*.collections.${AppwriteEnvironment.tweetCollection}.documents.*.create',
+                    )) {
+                      tweets.insert(0, Tweet.fromMap(data.payload));
+                    }
+                    return ListView.builder(
+                      itemCount: tweets.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        final tweet = tweets[index];
+                        return TweetCard(tweet: tweet);
+                      },
+                    );
+                  },
+                  error: (error, stackTrace) =>
+                      ErrorText(errorText: error.toString()),
+                  loading: () => const Loader(),
+                );
           },
           error: (error, stackTrace) => ErrorText(errorText: error.toString()),
           loading: () => const Loader(),
